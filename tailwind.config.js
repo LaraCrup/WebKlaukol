534a041a34885da59687cd2ab116e7095a455055
Lio <liobensignor@gmail.com>
2025-04-16 14:49:29 -0300

    Fix responsive tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,12 @@
 export default {
     theme: {
+        screens: {
+            'sm': '480px',
+            'md': '660px',
+            'lg': '850px',
+            'xl': '1080px',
+            '2xl': '1440px',
+        },
         extend: {
             colors: {
                 error: "#ff0000",
