5cf060c10e114ef405a6ff1102c41fd819f9346d
Emilio Bensignor <liobensignor@gmail.com>
2025-08-04 22:06:46 -0300

    Agregar videos a Evento

--- components/home/Evento.vue
+++ components/home/Evento.vue
@@ -1,14 +1,10 @@
 <template>
     <DefaultSection>
-        <DefaultContent class="md:max-h-[860px] lg:max-h-[780px] xl:max-h-[960px] md:flex-row">
-            <!-- Cambiar por video -->
-            <picture class="md:w-1/2">
-                <source media="(min-width: 1440px)" srcset="/videos/evento/Evento-Klaukol-DesktopXL.png" />
-                <source media="(min-width: 1080px)" srcset="/videos/evento/Evento-Klaukol-Desktop.png" />
-                <source media="(min-width: 660px)" srcset="/videos/evento/Evento-Klaukol-Tablet.png" />
-                <NuxtImg src="/videos/evento/Evento-Klaukol-Mobile.png" alt="Evento Klaukol"
-                    class="w-full h-full object-cover" />
-            </picture>
+        <DefaultContent class="md:h-[720px] lg:h-[800px] xl:h-[900px] 2xl:h-[1000px] md:flex-row">
+            <video class="md:w-1/2 w-full h-full object-cover" autoplay muted loop playsinline>
+                <source src="/videos/evento/Evento-Klaukol.mp4" type="video/mp4" media="(min-width: 660px)" />
+                <source src="/videos/evento/Evento-Klaukol-Mobile.mp4" type="video/mp4" />
+            </video>
             <div class="md:w-1/2 flex justify-center items-center p-4 md:px-8 xl:p-8 xl:pr-16">
                 <div class="flex flex-col gap-2 xl:gap-3 bg-lightGray rounded-[1.125rem] shadow-1 p-4 xl:p-6">
                     <DefaultH2 class="text-primary">
